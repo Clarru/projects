0a0f77d624644196938954f98e167c43f5d33481
Clarru <clauuu.marin@yahoo.com>
2019-01-18 17:39:52 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,24 @@
+function clock() {
+	const fullDate = new Date();
+	var hours = fullDate.getHours();
+	var mins = fullDate.getMinutes();
+	var seconds = fullDate.getSeconds();
+
+	if (hours < 10) {
+		hours = '0' + hours;
+	}
+	if (mins < 10) {
+		mins = '0' + mins;
+	}
+	if (seconds < 10) {
+		seconds = '0' + seconds;
+	}
+
+
+
+	document.getElementById('hour').innerHTML = hours + ' :';
+	document.getElementById('minute').innerHTML = mins + ' :';
+	document.getElementById('second').innerHTML = seconds;
+}
+
+setInterval (clock, 1000);
